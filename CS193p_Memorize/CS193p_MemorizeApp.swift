c519fd32fb5f475cc4d4696c17b27cb54b57d9cb
Lucas Marra <marralucas1@gmail.com>
2024-11-06 11:35:34 -0300

    Initial Commit

---
+++ CS193p_Memorize/CS193p_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_MemorizeApp.swift
+//  CS193p_Memorize
+//
+//  Created by Lucas Marra on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
